d38016fb26663eb83f5888e64cca9253551720ea
cjbc12 <cjbc122@gmail.com>
2017-01-27 14:02:14 -0600

    Update SPPCBarraganCA032.java

--- src/spp/cbarraganc/a03/pkg2/SPPCBarraganCA032.java
+++ src/spp/cbarraganc/a03/pkg2/SPPCBarraganCA032.java
@@ -1,20 +1,41 @@
-package spp.cbarraganc.a03.pkg2;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sppcbarraganca03.pkg1;
+import java.util.*;
 
 /**
  *
  * @author Barra
  */
-public class SPPCBarraganCA032 {
+public class SPPCBarraganCA031 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-            //returns square root of 9, i.e. 3
-     System.out.println(Math.sqrt(9));
-   
-     //returns square root of 25.5
-     System.out.println(Math.sqrt(25.5));
-    }
+        System.out.println("*************************");
+        System.out.println("Bienvenido al calculo de raíz cuadrada");
     
+    int a;
+     a= SolicitarNumero();
+     mostrar(a);
+    }
+    public static int SolicitarNumero(){
+        int a;
+        Scanner teclado=new Scanner(System.in);
+        System.out.println("Introduce Numero Entero");
+        a=teclado.nextInt();
+        return a;
+    }
+    public static void mostrar (int a){
+        double r;
+        r= Math.sqrt(a);
+        System.out.println("La Raiz es " + r);
+        System.out.println("Se utilizo (Math.sqrt)");
+    }
 }
+
+    
